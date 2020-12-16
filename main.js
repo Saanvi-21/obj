94ff92251e95f89d62f234fc273c138f843f7b5f
saanvi <71885805+Saanvi-21@users.noreply.github.com>
2020-12-16 19:57:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function bottel(){
+    window.location="a.html";
+}
+
+function almirah(){
+    window.location="d.html";
+}
+
+function chair(){
+    window.location="c.html";
+}
+
+function person(){
+    window.location="b.html";
+}
+
